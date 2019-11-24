4311a30cd35b1b68538bf1a9917310cd0699beef
Akhil Naveen <Akhil Naveen@192.168.0.8>
2019-11-25 00:37:26 +0530

    Variavles

---
+++ src/Variables.java
@@ -0,0 +1,41 @@
+
+public class Variables {
+
+/*A variable which is declared inside class - Instance
+	int a= 2;
+	float f= 10.0f;
+	
+	public static void main(String[] args) {
+		
+		Variables var = new Variables();
+		System.out.println(var.a);
+		System.out.println(var.f);
+		// TODO Auto-generated method stub
+*/
+	
+/* 	A variable which is declared inside method - Local
+void m1()
+	{
+	int a=30;
+	char c='N';
+	System.out.println(a);
+	System.out.println(c);
+
+	}
+	public static void main(String[] args) 
+	{
+		Variables var = new Variables();
+	var.m1();
+	
+	}
+*/
+	
+	//	A variable which is declared static keyword - Static Variable
+	static int a=30;
+	static char c='N';
+	public static void main(String[] args) 
+	{
+	System.out.println(a);
+	System.out.println(c);
+	}
+}
