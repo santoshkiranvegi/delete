8eb450655b92e52473de65fe653bd9a5dd453824
Akhil Naveen <Akhil Naveen@192.168.0.11>
2019-12-02 02:10:18 +0530

    commit 2

---
+++ src/Identifier.java
@@ -0,0 +1,25 @@
+
+public class Identifier {
+	
+	// A name in the java program is called Identifier, It maybe a method name, Variable Name & Class Name
+	//Basic Rules - A to Z, a to z, 0 to 9,Special Characters are under score (_) and dollar $
+// Identifiers are not allowed to start with Number
+	
+	void m1()
+	{
+		
+	}
+	/*void 1m2()
+	{
+	}
+	}*/
+	public static void main(String[] args) {
+		int x= 10;
+		/*int 1x =20;*/
+		
+		
+		
+
+	}
+
+}
